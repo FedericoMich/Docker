3ae78f49778f07520aedf44cb9d9c6f9b4bc3ea3
federico <federico.mich97@gmail.com>
2022-04-21 10:42:17 +0200

    fix package json name

--- CMYKtoHSL/src/controller/HttpController.ts
+++ CMYKtoHSL/src/controller/HttpController.ts
@@ -1,11 +1,11 @@
-import {convert} from '../service/Service';
+import {CmykToHsl} from '../service/Service';
 import {Express} from 'express';
 
 class HttpController {
     constructor(server: Express) {
         server.get('/', (req, res) => {
-            const color = JSON.parse(req.query.color) as ColorModel;
-            const convertedColor: ColorModel = convert(color);
+            const color = JSON.parse(req.query.color as any);
+            const convertedColor: any = CmykToHsl(color);
 
             res.send(convertedColor);
         });
--- CMYKtoHSL/src/service/Service.ts
+++ CMYKtoHSL/src/service/Service.ts
@@ -1,19 +1,8 @@
 import colorConverter from 'color-convert'
-import { Cmyk, Hex } from '../model/color'
 
 
-const urlParams = new URLSearchParams(window.location.search);
-const Value1 = urlParams.get('value1');
-const v1: number = +Value1;
-const Value2 = urlParams.get('value2');
-const Value3 = urlParams.get('value3');
-const Value4 = urlParams.get('value4');
+export const CmykToHsl=(color:any)=> {
 
-const convert = (color: Cmyk) => {
-    const colorToConvert: Cmyk = {cyan: v1, magenta: 0, yellow: 100, black: 0};
-    const convertedColor = colorConverter;
-    return {
-        hexValue: {hex: '00FF40'}
-    } 
+    colorConverter.cmyk.hsl(color)
 
 }
