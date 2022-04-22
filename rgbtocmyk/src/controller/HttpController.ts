395d77566b6f2a77159d384b70a53762f8c6582f
federico <federico.mich97@gmail.com>
2022-04-22 15:38:44 +0200

    implementation RGB to CMYK microservice

--- rgbtocmyk/src/controller/HttpController.ts
+++ rgbtocmyk/src/controller/HttpController.ts
@@ -1,11 +1,11 @@
-import { HextoRgb } from '../service/Service';
+import { RgbToCmyk } from '../service/Service';
 import { Express } from 'express';
 
 class HttpController {
     constructor(server: Express) {
         server.get('/', (req, res) => {
-            const color = (req.query.color as any).toString();
-            const convertedColor = HextoRgb(color);
+            const color = JSON.parse(req.query.color as any);
+            const convertedColor = RgbToCmyk(color);
 
             console.log(convertedColor)
             res.send(convertedColor);
